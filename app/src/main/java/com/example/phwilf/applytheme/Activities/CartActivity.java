bc13b8f065ddbd30b74fceb33be8957363e8736f
Parker <pHabelson@gmail.com>
2016-04-09 16:11:06 -0400

    Cart opens map to ToysRUs

--- app/src/main/java/com/example/phwilf/applytheme/Activities/CartActivity.java
+++ app/src/main/java/com/example/phwilf/applytheme/Activities/CartActivity.java
@@ -1,12 +1,16 @@
 package com.example.phwilf.applytheme.Activities;
 
 import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.phwilf.applytheme.Adapter.RecyclerAdapter;
@@ -26,6 +30,7 @@ public class CartActivity extends AppCompatActivity {
     private static final String Tag = CartActivity.class.getSimpleName();
 
     public static TextView items, totalPrice;
+    public static ImageView img_toMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,9 +45,31 @@ public class CartActivity extends AppCompatActivity {
 
         items = (TextView) findViewById(R.id.txt_totCartItems);
         totalPrice = (TextView) findViewById(R.id.txt_totCartPrice);
+        img_toMap = (ImageView) findViewById(R.id.img_toMap);
+
+        img_toMap.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // your code here
+                toMaps_ToysRUs();
+            }
+        });
+
         items.setText("Items: " + Cart.items);
         totalPrice.setText("Total Price: $" + Cart.totalPrice);
     }
+
+    //general private function for calling URLs
+    private void toUrl(String url){
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
+    }
+
+    //specifc function for loading google maps
+    public void toMaps_ToysRUs(){
+        toUrl("https://www.google.com/maps/place/Toys%E2%80%9CR%E2%80%9DUs/@40.9794274,-74.2563207,17z/data=!4m7!1m4!3m3!1s0x89c302dac069cbdf:0x7a8c72d741957102!2s1+Geoffrey+Way,+Wayne,+NJ+07470!3b1!3m1!1s0x89c302cde7d27bf1:0x2ad7dabe9764018e");
+    }
+
 
     private void setUpRecyclerView(ArrayList<Toy> toyList){
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.cartRecyclerView);
